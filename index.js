28bf42317128e5541e5419474e976f94e970f07f
revanth892 <revanth9347569169@gmail.com>
2024-05-18 03:33:48 +0530

    added the redis configuration

--- index.js
+++ index.js
@@ -1,42 +1,73 @@
-const express= require("express")
-const axios = require('axios')
+const express = require("express");
+const axios = require("axios");
+const { createClient } = require("redis");
+
 const app = express();
 const PORT = 6060;
 
+// Initialize Redis client with the provided connection details
+const client = createClient({
+  password: 'CbfmTPmzYJbg0tVKYqoS7JxQwjhJOHnI',
+  socket: {
+    host: 'redis-17549.c330.asia-south1-1.gce.redns.redis-cloud.com',
+    port: 17549
+  }
+});
 
+client.on("error", (error) => {
+  console.error(`Redis client error: ${error}`);
+  process.exit(1); // Exit the process if Redis connection fails
+});
 
-const fetchApiData =async(pin)=>{
-    const apiResponse = await axios.get(
-        `http://api.zippopotam.us/us/${pin}`
-    );
-    console.log("Request sent to the API")
-    // console.log(apiResponse.data)
-    return apiResponse.data
-}
+client.on("connect", () => {
+  console.log("Connected to Redis");
 
-const getPinData=async(req,res)=>
-{
-    const pin = req.params.pin;
+  // Start the Express server after successful Redis connection
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+});
+
+(async () => {
+  await client.connect();
+})();
+
+const fetchApiData = async (pin) => {
+  try {
+    const apiResponse = await axios.get(`http://api.zippopotam.us/us/${pin}`);
+    console.log("Request sent to the API");
+    return apiResponse.data;
+  } catch (error) {
+    console.error("Error fetching data from API:", error);
+    throw error;
+  }
+};
+
+const getPinData = async (req, res) => {
+  const pin = req.params.pin;
   let results;
+  let isCached = false;
 
   try {
-    results = await fetchApiData(pin);
-    if (results.length === 0) {
-      throw "API returned an empty array";
+    const cacheResults = await client.get(pin);
+    if (cacheResults) {
+      isCached = true;
+      results = JSON.parse(cacheResults);
+    } else {
+      results = await fetchApiData(pin);
+      if (!results || Object.keys(results).length === 0) {
+        throw new Error("API returned an empty object");
+      }
+      await client.set(pin, JSON.stringify(results));
     }
     res.send({
-      fromCache: false,
+      fromCache: isCached,
       data: results,
     });
   } catch (error) {
-    console.error(error);
+    console.error("Error processing request:", error);
     res.status(404).send("Data unavailable");
   }
-}
-
+};
 
 app.get("/us/:pin", getPinData);
-
-app.listen(PORT,()=>{
-    console.log('server is running')
-})
